cf6b44fd3bcf5602df4e598406d8c56a3d7ca9f8
kirikx <kirikx@mail.ru>
2019-10-21 21:32:49 +0300

    ДЗ 5

--- src/code/TestTread.java
+++ src/code/TestTread.java
@@ -1,40 +1,82 @@
 package code;
-import java.util.Arrays;
-/*
-        1. Необходимо написать два метода, которые делают следующее:
-        1) Создают одномерный длинный массив, например:
-        static final int size = 10000000;
-        static final int h = size / 2;
-        float[] arr = new float[size];
-        2) Заполняют этот массив единицами;
-        3) Засекают время выполнения: long a = System.currentTimeMillis();
-        4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
-        arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-        5) Проверяется время окончания метода System.currentTimeMillis();
-        6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a);
-        Отличие первого метода от второго:
-        Первый просто бежит по массиву и вычисляет значения.
-        Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает эти массивы обратно в один.
 
-        Пример деления одного массива на два:
-        System.arraycopy(arr, 0, a1, 0, h);
+
+public class TestTread {
+    static final int size = 10_000_000;
+    static final int h = size / 2;
+
+    public static void main(String[] args) {
+        float[] arr = new float[size];
+        float[] arr1 = new float[size];
+        float[] arr2 = new float[size];
+
+        arrayToOne(arr);
+        long a = System.currentTimeMillis();
+
+        System.out.println("Расчет в одном потоке");
+        var1(arr);
+        System.out.println("Время выполнения преобразования " + (System.currentTimeMillis() - a));
+        System.arraycopy(arr, 0, arr1, 0, size); // Копируем массив для сравнения
+
+        System.out.println();
+
+        arrayToOne(arr);
+        System.out.println("Расчет в двух потоках");
+        a = System.currentTimeMillis();
+        var2(arr);
+        System.out.println("Время выполнения преобразования " + (System.currentTimeMillis() - a));
+        System.arraycopy(arr, 0, arr2, 0, size); // Копируем массив для сравнения
+
+        System.out.println("Проверка на равенство массивов: " + checkArray(arr1, arr2));
+
+    }
+
+    private static void arrayToOne(float arr[]) {
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = 1;
+    }
+
+    private static void var1(float arr[]) {
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+    }
+
+    private static void convert(float arr[], int h) { // Применение конвертера со смещением позволяет добится идентичности результатов вычисления
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + h) / 5) * Math.cos(0.2f + (i + h) / 5)
+                    * Math.cos(0.4f + (i + h) / 2));
+        }
+    }
+    private static void var2(float arr[]) {
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+
+        System.arraycopy(arr, 0, a1, 0, h); // Разделяем
         System.arraycopy(arr, h, a2, 0, h);
 
-        Пример обратной склейки:
-        System.arraycopy(a1, 0, arr, 0, h);
-        System.arraycopy(a2, 0, arr, h, h);
-
-        Примечание:
-        System.arraycopy() копирует данные из одного массива в другой:
-        System.arraycopy(массив-источник, откуда начинаем брать данные из массива-источника, массив-назначение, откуда начинаем записывать данные в массив-назначение, сколько ячеек копируем)
-        По замерам времени:
-        Для первого метода надо считать время только на цикл расчета:
-        for (int i = 0; i < size; i++) {
-        arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        Thread t1 = new Thread(() -> TestTread.convert(a1, 0));
+        Thread t2 = new Thread(() -> TestTread.convert(a2, h));
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return;
         }
-        Для второго метода замеряете время разбивки массива на 2, просчета каждого из двух массивов и склейки.
-*/
-public class TestTread {
+
+        System.arraycopy(a1, 0, arr, 0, h); // Склеиваем
+        System.arraycopy(a2, 0, arr, h, h);
+    }
+
+    private static boolean checkArray(float arr1[], float arr2[]) {
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i])
+                return false;
+        }
+        return true;
+    }
 
 }
 
